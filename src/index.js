cc68598b5a0971137348825e7e60be388af4abbf
Parv-gugnani <9765.parv@gmail.com>
2023-10-18 14:59:21 +0530

    io

--- src/index.js
+++ src/index.js
@@ -12,8 +12,10 @@ const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
-io.on("connection", () => {
+io.on("connection", (socket) => {
   console.log("new websocket connections");
+
+  socket.emit("countUpdated");
 });
 
 server.listen(port, () => {
