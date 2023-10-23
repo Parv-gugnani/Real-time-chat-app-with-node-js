abc90fc35a2383b9d9ea6567a52be61fdc16d538
Parv-gugnani <9765.parv@gmail.com>
2023-10-23 14:48:30 +0530

    index js

--- src/index.js
+++ src/index.js
@@ -20,11 +20,11 @@ io.on("connection", (socket) => {
 
   socket.emit("countUpdated", count);
 
-  socket.on("increment", () => {
-    count++;
-    // socket.emit("countUpdated", count);
-    io.emit("countUpdated", count);
-  });
+  // socket.on("increment", () => {
+  //   count++;
+  //   // socket.emit("countUpdated", count);
+  //   io.emit("countUpdated", count);
+  // });
 });
 
 server.listen(port, () => {
