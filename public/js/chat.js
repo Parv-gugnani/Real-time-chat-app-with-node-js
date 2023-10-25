dfc992d05f40fef3df02012716e7ed046714f179
Parv-gugnani <9765.parv@gmail.com>
2023-10-25 15:18:25 +0530

    event

--- public/js/chat.js
+++ public/js/chat.js
@@ -11,28 +11,32 @@ document.querySelector("#message-form").addEventListener("submit", (e) => {
 
   socket.emit("sendMessage", message, (error) => {
     if (error) {
-      return console.log(error);
+      return console.error(error);
     }
-
-    console.log("Message delivered!");
+    console.log("Message was sent successfully");
   });
 });
 
+// socket.on("countUpdated", (count) => {
+//   console.log("The count has been updated!", count);
+// });
+
+// document.querySelector("#increment").addEventListener("click", () => {
+//   console.log("clicked");
+//   socket.emit("increment");
+// });
+
 document.querySelector("#send-location").addEventListener("click", () => {
   if (!navigator.geolocation) {
-    return alert("Geolocation is not supported.");
+    return alert("Geolocation not ssupported");
   }
 
   navigator.geolocation.getCurrentPosition((position) => {
-    socket.emit(
-      "sendLocation",
-      {
-        latitude: position.coords.latitude,
-        longitude: position.coords.longitude,
-      },
-      () => {
-        console.log("Location shared!");
-      }
-    );
+    // console.log(position);
+
+    socket.emit("sendLocation", {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
+    });
   });
 });
