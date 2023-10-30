126d49000143a0331e7e375105449a5f4b951023
Parv-gugnani <9765.parv@gmail.com>
2023-10-30 13:46:43 +0530

    fix

--- src/index.js
+++ src/index.js
@@ -59,5 +59,5 @@ io.on("connection", (socket) => {
 });
 
 server.listen(port, () => {
-  console.log(`Server is up on port ${port}!`);
+  console.log(`Server is up on port http://localhost:${port}`);
 });
