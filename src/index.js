12833930f0b47667d19a1f1f361513e98ea7452d
Parv-gugnani <9765.parv@gmail.com>
2023-10-30 18:16:13 +0530

    username fixed

--- src/index.js
+++ src/index.js
@@ -31,10 +31,13 @@ io.on("connection", (socket) => {
 
     socket.join(user.room);
 
-    socket.emit("message", generateMessage("Welcome!"));
+    socket.emit("message", generateMessage("Admin", "Welcome!"));
     socket.broadcast
       .to(user.room)
-      .emit("message", generateMessage(`${user.username} has joined!`));
+      .emit(
+        "message",
+        generateMessage("Admin", `${user.username} has joined!`)
+      );
 
     // confirming
     callback();
@@ -51,7 +54,7 @@ io.on("connection", (socket) => {
       return callback("Profanity is not allowed!");
     }
 
-    io.to(user.room).emit("message", generateMessage(message));
+    io.to(user.room).emit("message", generateMessage(user.username, message));
     callback();
   });
 
@@ -60,6 +63,7 @@ io.on("connection", (socket) => {
     io.to(user.room).emit(
       "locationMessage",
       generateLocationMessage(
+        user.username,
         `https://google.com/maps?q=${coords.latitude},${coords.longitude}`
       )
     );
@@ -71,7 +75,10 @@ io.on("connection", (socket) => {
 
     // if the user never joined or just skipped form outside then we dont want to send this message to every in the server
     if (user) {
-      io.emit("message", generateMessage(`${user.username} has left!`));
+      io.emit(
+        "message",
+        generateMessage("Admin", `${user.username} has left!`)
+      );
     }
   });
 });
